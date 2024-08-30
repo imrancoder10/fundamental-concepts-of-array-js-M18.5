d7a6765ee816b9c7772b73915103680b2c3d48ba
imrancoder10 <imranhossain1148@gmail.com>
2024-08-31 02:16:46 +0600

    array initialization

---
+++ array.js
@@ -0,0 +1,17 @@
+/*
+const num1=12;
+const num2=22;
+const num3=422;
+const num4=52;
+*/
+// ============JavaScript Array=============================
+// Array is a collection of multiple items under a single variable name.
+// Collection of numbers
+const numbers = [12, 22, 422, 52];
+// Collection of Name
+const friends = ['Amena', 'kamena', 'Jamena', 'Janina', 'Pabina'];
+// Collection of Boolean
+const passed = [true, false, false, true];
+// Collection of number, name/string, boolean, fraction number
+const mixedArray = [12, 'Jhaudanga', true, 12.86];
+
