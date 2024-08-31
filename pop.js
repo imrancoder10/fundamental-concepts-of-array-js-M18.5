9a02fc4de22b357183c26ea1d7755e0aac7eff95
imrancoder10 <imranhossain1148@gmail.com>
2024-08-31 19:51:20 +0600

    pop

---
+++ pop.js
@@ -0,0 +1,7 @@
+const students = ['Amena', 'Kamena', 'Jamina', 'Janina', 'kamina'];
+console.log(students);
+const lastName = students.pop();
+console.log(lastName);
+// console.log(students);
+students.pop();
+// console.log(students);
