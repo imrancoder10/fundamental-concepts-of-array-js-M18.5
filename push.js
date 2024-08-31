6c6744f428449aa8640437026e42c6ad210ae063
imrancoder10 <imranhossain1148@gmail.com>
2024-08-31 19:40:05 +0600

    add any element in the array list using push

---
+++ push.js
@@ -0,0 +1,7 @@
+const ages = [];
+const numbers = [50, 44, 12, 23, 56, 20];
+// ***অ্যারের মধ্যে কোনো এলিমেন্ট যোগ করতে চাইলে পুশ ব্যবহার করা হয়।****
+console.log(numbers);
+numbers.push(111);
+numbers.push(33, 42, 55);
+console.log(numbers);
