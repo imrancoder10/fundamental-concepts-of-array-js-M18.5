50452c2aab8a752315bf24d2ba589c49af542f69
imrancoder10 <imranhossain1148@gmail.com>
2024-09-01 14:10:12 +0600

    concat

---
+++ concat.js
@@ -0,0 +1,12 @@
+const age = [12, 23, 50, 88, 55, 45];
+const weight = [70, 80, 90, 98, 50];
+console.log(age.concat(weight));
+
+//==========================================
+
+const boyRoll = [20, 21, 22, 23];
+const girlRoll = [101, 102, 103, 104];
+
+console.log(boyRoll.concat(girlRoll));
+
+//======================================================
