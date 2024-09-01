f4af8ca86b5bb5b4b8e3ed425b7a842cfdc6dc58
imrancoder10 <imranhossain1148@gmail.com>
2024-09-01 12:51:23 +0600

    isArray

--- isArray.js
+++ isArray.js
@@ -2,8 +2,10 @@ const friends = ['Amena', 'Kamena', 'Jamena', 'Pabena', 'Nibena'];
 const nums = [];
 const food = 'Ros o gol l a';
 const age = 15;
+const inviteMe = true;
 
-console.log(Array.isArray(friends)); //true
-console.log(Array.isArray(nums)); //true
-console.log(Array.isArray(food)); //false
-console.log(Array.isArray(age)); //false
+console.log(Array.isArray(friends)); // true
+console.log(Array.isArray(nums)); // true
+console.log(Array.isArray(food)); // false
+console.log(Array.isArray(age)); // false
+console.log(Array.isArray(inviteMe)); // false
